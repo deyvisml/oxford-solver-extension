cf0626683395eca0a9e397ff99b0976f2d92e3c8
Deyvis ML <deyvisml2002@gmail.com>
2023-03-02 18:40:01 -0500

    Interfaces terminadas, falta la float window de las answers

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,21 +1,378 @@
 const body_element = document.querySelector("body");
 
-const create_interface = () => {
+const display_login_interface = () => {
+  // Elminar la interfaz actual
+  document.querySelector(".interface")?.remove();
+
   const interface_element = document.createElement("div");
   interface_element.classList.add("interface");
 
-  interface_element.innerHTML = `<div class="btn-key-container">
-                                <img src="${chrome.runtime.getURL(
-                                  "images/key.png"
-                                )}" alt="">
-                                </div>
-                                <ul class="options-container">
-                                <li><i class="fa-brands fa-google"></i></li>
-                                <li><i class="fa-solid fa-code"></i></i></li>
-                                <li><i class="fa-solid fa-cart-shopping"></i></li>
+  interface_element.innerHTML = `<ul class="options-container only-login">
+                                  <li title="Login" class="login-btn"><i class="fa-brands fa-google"></i></li>
                                 </ul>`;
 
   body_element.insertAdjacentElement("afterbegin", interface_element);
+
+  const login_btn = document.querySelector(".login-btn");
+
+  login_btn.addEventListener("click", () => {
+    handler_login_btn();
+  });
 };
 
-create_interface();
+const display_user_interface = () => {
+  // Elminar la interfaz actual
+  document.querySelector(".interface")?.remove();
+
+  const interface_element = document.createElement("div");
+  interface_element.classList.add("interface");
+
+  interface_element.innerHTML = `<ul class="options-container">
+                                  <li title="Perfil" class="perfil-btn"><i class="fa-solid fa-user"></i></li>
+                                  <li title="Comprar código" class="buy-code-btn"><i class="fa-solid fa-cart-shopping"></i></li>
+                                  <li title="Ingresar código" class="display-enter-code-btn"><i class="fa-solid fa-code"></i></li>
+                                </ul>`;
+
+  body_element.insertAdjacentElement("afterbegin", interface_element);
+
+  const perfil_btn = document.querySelector(".perfil-btn");
+  perfil_btn.addEventListener("click", () => {
+    handler_display_perfil();
+  });
+
+  const enter_code_btn = document.querySelector(".display-enter-code-btn");
+  enter_code_btn.addEventListener("click", () => {
+    handler_display_enter_code();
+  });
+
+  const buy_code_btn = document.querySelector(".buy-code-btn");
+  buy_code_btn.addEventListener("click", () => {
+    const url = "https://wa.me/message/HRSSF2L4HUIEA1";
+    window.open(url, "_blank").focus();
+  });
+};
+
+const display_key_btn = async () => {
+  const response = await verify_activity();
+
+  if (response.activity_available === true) {
+    const key_btn = document.createElement("div");
+    key_btn.classList.add("btn-key-container");
+    key_btn.setAttribute("title", "Mostrar respuestas");
+    key_btn.innerHTML = `<img src="${chrome.runtime.getURL(
+      "images/key.png"
+    )}" alt="">`;
+
+    const interface_element = document.querySelector(".interface");
+    interface_element.insertAdjacentElement("afterbegin", key_btn); // issue: but it's working xd it should be beforebegin
+
+    const id_activity = response.id_activity;
+    console.log("id_activity", id_activity);
+
+    // add listener
+    key_btn.addEventListener("click", () => {
+      handler_key_btn(id_activity);
+    });
+  }
+};
+
+const verify_activity = async () => {
+  // TODO: Estas rutas xpath son muy IMPORTANTES ya que definen con exactitud de que activity se trata. (puede que varien en las distintas actitivy ...)
+  const path_topic =
+    "/html/body/div[3]/div[2]/div[1]/div/div[1]/div/div/d2l-breadcrumbs/d2l-breadcrumb[3]";
+  const path_activity_type =
+    "/html/body/div[3]/div[2]/div[1]/div/div[1]/div/div/d2l-breadcrumbs/d2l-breadcrumb-current-page";
+
+  const topic_element = await checkElement(path_topic);
+  const activity_type_element = await checkElement(path_activity_type);
+
+  const topic_name = topic_element.getAttribute("text");
+  const activity_type_name = activity_type_element.getAttribute("text");
+
+  // verificar en el backend
+  // https://stackoverflow.com/questions/52087734/make-promise-wait-for-a-chrome-runtime-sendmessage
+  const response = await new Promise((resolve) => {
+    chrome.runtime.sendMessage(
+      {
+        action: "verify-activity",
+        topic_name: topic_name,
+        activity_type_name: activity_type_name,
+      },
+      resolve
+    );
+  });
+
+  return response;
+};
+
+/* =================================== */
+/**
+ * Metodo a ejecutar por defecto, para mostrar la interfaz correspondiente
+ */
+const main = () => {
+  chrome.runtime.sendMessage(
+    { action: "get-status-token" },
+    function (response) {
+      if (response.token_alive === false) {
+        display_login_interface();
+      } else {
+        display_user_interface();
+      }
+      display_key_btn();
+    }
+  );
+};
+
+main();
+
+/* ====================================================================== */
+/* == HANDLERS == TODO: Los handlers son metodos de escuah del addeventlistener*/
+
+const handler_key_btn = (id_activity) => {
+  chrome.runtime.sendMessage(
+    {
+      action: "get-answers",
+      id_activity: id_activity,
+    },
+    function (response) {
+      console.log(":::: SIUU", response);
+      if ("token_alive" in response && response.token_alive === false) {
+        // verificar si el usuario esta logeado (token)
+        alert("Por favor, primero debe iniciar sesión.");
+      } else if (
+        // verificar si el usuario tiene un código activo
+        "code_available" in response &&
+        response.code_available === false
+      ) {
+        alert("Usted no cuenta con un código vigente.\n");
+      } else {
+        // si todo esta ok mostrar ventana flotante
+        // TODO: WORKING..
+
+        alert(id_activity);
+      }
+    }
+  );
+};
+
+const handler_display_enter_code = () => {
+  // display flaot window
+  const float_window_element = document.createElement("div");
+  float_window_element.classList.add("float-window");
+  float_window_element.innerHTML = `<div class="head-float-window">
+                                      <div class="close-float-window">
+                                        <i class="fa-solid fa-xmark"></i>
+                                      </div>
+                                    </div>
+                                    <div class="body-float-window">
+                                      <p class="title-window">
+                                          Ingresar código
+                                      </p>
+                                      <p class="description-window">
+                                          Por favor, ingrese su código adquirido en la parte de abajo
+                                      </p>
+                                      <div class="center-input-button">
+                                          <input class="input-code no-spinner" type="text" maxlength="8" placeholder="00000000" autofocus>
+                                          <button class="btn-register-code">CANJEAR</button>
+                                      </div>
+                                    </div>`;
+
+  body_element.insertAdjacentElement("beforebegin", float_window_element);
+
+  const cover_window_element = document.createElement("div");
+  cover_window_element.classList.add("cover-window");
+  body_element.insertAdjacentElement("beforebegin", cover_window_element);
+
+  // add events (close window)
+  cover_window_element.addEventListener("click", () => {
+    handler_close_float_window();
+  });
+  const close_float_window_btn = document.querySelector(".close-float-window");
+  close_float_window_btn.addEventListener("click", () => {
+    handler_close_float_window();
+  });
+
+  // add events (others)
+  const register_code_btn = document.querySelector(".btn-register-code");
+  register_code_btn.addEventListener("click", () => {
+    // obtener el codigo ingresado
+    const code = document.querySelector(".input-code").value;
+    handler_register_code(code);
+  });
+};
+
+const handler_register_code = (code) => {
+  // verificar si el codigio es valido
+  if (code.length === 8 && isNumeric(code)) {
+    chrome.runtime.sendMessage(
+      {
+        action: "register-code",
+        code: code,
+      },
+      function (response) {
+        if ("token_alive" in response && response.token_alive === false) {
+          // token alive = false
+          // eliminar la ventana flotante
+          handler_close_float_window();
+          // cambiar a la interfaz de login
+          display_login_interface();
+          // TODO: Clear storage (to delete the token) - Aunque esto creo ya se hace en el background cuando establece el falor de token_alive = false
+        } else if ("code_registered" in response) {
+          let badge = null;
+          if (response.code_registered === true) {
+            // code registered = true
+            document.querySelector(".response-register-code")?.remove();
+            badge = document.createElement("p");
+            badge.classList.add("response-register-code", "success");
+            badge.innerText = `Código registrado exitosamente! (expira: ${response.expire_at})`;
+          } else {
+            // code registered = false
+            document.querySelector(".response-register-code")?.remove();
+            badge = document.createElement("p");
+            badge.classList.add("response-register-code", "error");
+            badge.innerText = `Error, el código no es válido o ya fue usado.`;
+          }
+          const body_float_window_element =
+            document.querySelector(".body-float-window");
+          body_float_window_element.insertAdjacentElement("beforeend", badge); // insertando la bandera
+          window.getComputedStyle(badge);
+        }
+        console.log("Response register code:", response);
+      }
+    );
+  } else {
+    alert("Por favor, ingrese un código valido");
+  }
+};
+
+const handler_login_btn = () => {
+  chrome.runtime.sendMessage({ action: "login" }, function (response) {
+    if (response.logged) {
+      main();
+    }
+  });
+};
+
+const handler_display_perfil = () => {
+  // TODO: Obtener la información del usuario (depaso se verifica si el token sigue activo)
+  chrome.runtime.sendMessage(
+    { action: "get-user-profile" },
+    function (response) {
+      const error = chrome.runtime.lastError;
+      if (error) {
+        console.error(error);
+      }
+
+      console.log("reponse content", response);
+
+      if (response.token_alive === false) {
+        //TODO: El usuario necesita logearse
+        display_login_interface();
+      } else {
+        const float_window_element = document.createElement("div");
+        float_window_element.classList.add("float-window");
+        float_window_element.innerHTML = `<div class="head-float-window">
+                                            <div class="close-float-window">
+                                             <i class="fa-solid fa-xmark"></i>
+                                            </div>
+                                          </div>
+                                          <div class="body-float-window">
+                                            <p class="title-window">Información personal</p>
+                                            <p class="item-profile-description first">
+                                              <strong>Nombre: </strong>${
+                                                response.user_info.name ??
+                                                "null"
+                                              }
+                                            </p>
+                                            <p class="item-profile-description">
+                                              <strong>Código registrado: </strong>${
+                                                response.user_info.used_at ??
+                                                "null"
+                                              }
+                                            </p>
+                                            <p class="item-profile-description">
+                                              <strong>Código expira: </strong>${
+                                                response.user_info.expire_at ??
+                                                "null"
+                                              }
+                                            </p>
+      
+                                            <button class="close-session-btn">Cerrar sesión</button>
+                                          </div>`;
+
+        body_element.insertAdjacentElement("beforebegin", float_window_element);
+
+        const cover_window_element = document.createElement("div");
+        cover_window_element.classList.add("cover-window");
+        body_element.insertAdjacentElement("beforebegin", cover_window_element);
+
+        cover_window_element.addEventListener("click", () => {
+          handler_close_float_window();
+        });
+        const close_float_window_btn = document.querySelector(
+          ".close-float-window"
+        );
+        close_float_window_btn.addEventListener("click", () => {
+          handler_close_float_window();
+        });
+
+        const close_sessión_btn = document.querySelector(".close-session-btn");
+        close_sessión_btn.addEventListener("click", () => {
+          handler_close_session_btn();
+        });
+      }
+    }
+  );
+};
+
+const handler_close_session_btn = () => {
+  // enviar mensaje close session
+  chrome.runtime.sendMessage(
+    {
+      action: "close-session",
+    },
+    function (response) {}
+  );
+
+  // eliminar la ventana flotante
+  handler_close_float_window();
+
+  // cambiar a la interfaz de login
+  main();
+};
+
+const handler_close_float_window = () => {
+  const float_window_element = document.querySelector(".float-window");
+  const cover_window_element = document.querySelector(".cover-window");
+
+  float_window_element.remove();
+  cover_window_element.remove();
+
+  body_element.classList.remove("stop-scrolling");
+};
+
+/* ====================================================================== */
+/* === METODOS AUXILIARES === */
+
+const getElementByXpath = (path) => {
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+};
+
+const checkElement = async (selector) => {
+  console.log("SEARCHING ELEMENT");
+  while (getElementByXpath(selector) === null) {
+    console.log("PIPIPI");
+    await new Promise((resolve) => requestAnimationFrame(resolve));
+  }
+  return getElementByXpath(selector);
+};
+
+const isNumeric = (value) => {
+  return /^\d+$/.test(value);
+};
---
+++ service-worker.js
@@ -0,0 +1,359 @@
+chrome.runtime.onInstalled.addListener(function (details) {
+  clear_cachetoken_and_storage();
+});
+
+/**
+ * LImpiar el storage local y la cache de google auth (esto para permitir al usuario ingresar una cuenta cualquiera)
+ */
+const clear_cachetoken_and_storage = () => {
+  /*chrome.storage.local.clear(function () {
+    const error = chrome.runtime.lastError;
+    if (error) {
+      console.error(error);
+    }
+    // do something more
+  });*/
+
+  chrome.identity.clearAllCachedAuthTokens((res) => {
+    console.log(res);
+  });
+};
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(message, sender, sendResponse);
+
+  if (message.action === "login") {
+    // https://stackoverflow.com/questions/53024819/chrome-extension-sendresponse-not-waiting-for-async-function/53024910#53024910
+    (async () => {
+      // Verificar si hay una sesión existente
+      const result = await get_status_token();
+
+      if (result.token_alive === false) {
+        /**
+         * === GOOGLE LOGIN START ===
+         */
+        chrome.identity.getAuthToken({ interactive: true }, async (token) => {
+          if (chrome.runtime.lastError) {
+            clear_cachetoken_and_storage();
+            sendResponse({
+              logged: false,
+            });
+            return;
+          }
+
+          const response = await fetch(
+            `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`
+          );
+          const data = await response.json();
+
+          const result = await login(data);
+
+          if (!Object.keys(result).length || result.logged === false) {
+            clear_cachetoken_and_storage();
+            sendResponse({
+              logged: false,
+            });
+          } else {
+            console.log("USUARIO LOGEADO");
+            chrome.storage.local.set(
+              {
+                token: result.token,
+              },
+              function () {
+                console.log("CREDENCIALES GUARDADAS");
+                sendResponse({
+                  logged: true,
+                });
+              }
+            );
+          }
+        });
+        /*END GOOGLE LOGIN*/
+      } else {
+        sendResponse({ logged: true });
+      }
+    })();
+
+    return true; // para mantener el canal de mensaje abierto: https://stackoverflow.com/questions/20077487/chrome-extension-message-passing-response-not-sent
+  } else if (message.action === "get-user-profile") {
+    (async () => {
+      // https://stackoverflow.com/questions/53024819/chrome-extension-sendresponse-not-waiting-for-async-function/53024910#53024910
+      const result = await get_status_token();
+      console.log("RESUL 2:", result);
+      if (result.token_alive === true) {
+        const data = await get_user_profile();
+        console.log("RESUL 3:", data);
+        data.used_at = data.used_at.substr(0, 10);
+        data.expire_at = data.expire_at.substr(0, 10);
+        result.user_info = data;
+      }
+      sendResponse(result);
+    })();
+    return true;
+  } else if (message.action === "get-status-token") {
+    (async () => {
+      console.log("fun status token");
+      // https://stackoverflow.com/questions/53024819/chrome-extension-sendresponse-not-waiting-for-async-function/53024910#53024910
+      const result = await get_status_token();
+      console.log("result", result);
+      sendResponse(result);
+    })();
+    return true;
+  } else if (message.action === "close-session") {
+    clear_cachetoken_and_storage();
+    sendResponse({ session_closed: true });
+    return true;
+  } else if (message.action === "register-code") {
+    // verificar token
+    (async () => {
+      // https://stackoverflow.com/questions/53024819/chrome-extension-sendresponse-not-waiting-for-async-function/53024910#53024910
+      let result = await get_status_token();
+      console.log("RESUL 2:", result);
+      if (result.token_alive === true) {
+        //registrar codigo
+        result = await register_code(message.code);
+        if ("expire_at" in result) {
+          result.expire_at = result.expire_at.substr(0, 10);
+        }
+      }
+      sendResponse(result);
+    })();
+  } else if (message.action === "verify-activity") {
+    (async () => {
+      // https://stackoverflow.com/questions/53024819/chrome-extension-sendresponse-not-waiting-for-async-function/53024910#53024910
+      const result = await verify_activity(
+        message.topic_name,
+        message.activity_type_name
+      );
+      console.log("result", result);
+
+      sendResponse(result);
+    })();
+  } else if (message.action === "get-answers") {
+    (async () => {
+      // https://stackoverflow.com/questions/53024819/chrome-extension-sendresponse-not-waiting-for-async-function/53024910#53024910
+      let result = await get_status_token();
+      if (result.token_alive === true) {
+        result = await get_status_code();
+        if (result.code_available === true) {
+          // show answers
+          const id_activity = message.id_activity;
+          result = await get_answers(id_activity);
+        }
+      }
+      sendResponse(result);
+    })();
+  }
+
+  return true;
+});
+
+const get_answers = async (id_activity) => {
+  const result = await readLocalStorage("token");
+  const token = result.token;
+
+  const response = await fetch("http://127.0.0.1:8000/api/solve/", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      id_activity: id_activity,
+    }),
+  });
+
+  let data = await response.json();
+
+  if (!("succeed" in data)) {
+    data = { succeed: false };
+  }
+
+  return data;
+};
+
+const verify_activity = async (topic_name, activity_type_name) => {
+  console.log("names:", topic_name, activity_type_name);
+
+  const response = await fetch("http://127.0.0.1:8000/api/verify-activity/", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      topic_name: topic_name,
+      activity_type_name: activity_type_name,
+    }),
+  });
+
+  let data = await response.json();
+
+  console.log("data::", data);
+
+  if (!("activity_available" in data) || data["activity_available"] === false) {
+    data = { activity_available: false };
+  }
+
+  return data;
+};
+
+const register_code = async (code) => {
+  const result = await readLocalStorage("token");
+  const token = result.token;
+
+  const response = await fetch("http://127.0.0.1:8000/api/register-code/", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      code: code,
+    }),
+  });
+
+  let data = await response.json();
+
+  if (!("code_registered" in data)) {
+    data = { code_registered: false };
+  }
+
+  return data;
+};
+
+const login = async (credentials) => {
+  const response = await fetch("http://127.0.0.1:8000/api/login/", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: credentials.email,
+      password: "true",
+    }),
+  });
+  let data = await response.json();
+
+  if ("message" in data && data.message === "invalid credentials") {
+    data = await register(credentials);
+  } else if ("message" in data) {
+    // TODO: Si se trata de otro error
+    return { logged: false };
+  }
+
+  let result = {};
+  if (data.errors && Object.keys(data.errors).length) {
+    console.log("LOGIN FAIL: ERROS IN LOGIN OR REGISTER PROCCESS"); // TODO: No registrar ni logear usuario en este caso
+    result = { logged: false };
+  } else {
+    console.log("LOGIN OK", data);
+    result = { logged: true, token: data.token };
+  }
+
+  return result;
+};
+
+// registrar nuevo usuario
+const register = async (credentials) => {
+  console.log("REGISTERING USER");
+
+  const response = await fetch("http://127.0.0.1:8000/api/register/", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: credentials.name ?? "null",
+      email: credentials.email,
+      password: "true",
+      password_confirmation: "true",
+    }),
+  });
+  const data = await response.json();
+
+  return data;
+};
+
+const readLocalStorage = async (key) => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get([key], function (result) {
+      resolve(result);
+    });
+  });
+};
+
+const get_user_profile = async () => {
+  const result = await readLocalStorage("token");
+  const token = result.token;
+
+  const response = await fetch("http://127.0.0.1:8000/api/user-profile/", {
+    method: "get",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  let data = await response.json();
+
+  return data;
+};
+
+const get_status_token = async () => {
+  const result = await readLocalStorage("token");
+
+  const token = "token" in result ? result.token : "null";
+
+  let data = {};
+
+  if (token !== "null") {
+    const response = await fetch("http://127.0.0.1:8000/api/status-token/", {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    data = await response.json();
+
+    if ("message" in data) {
+      clear_cachetoken_and_storage();
+      data = { token_alive: false };
+    }
+  } else {
+    clear_cachetoken_and_storage();
+    data = { token_alive: false };
+  }
+
+  return data;
+};
+
+const get_status_code = async () => {
+  const result = await readLocalStorage("token");
+  const token = "token" in result ? result.token : "null";
+
+  let data = {};
+
+  if (token !== "null") {
+    const response = await fetch("http://127.0.0.1:8000/api/status-code/", {
+      method: "get",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    data = await response.json();
+  } else {
+    clear_cachetoken_and_storage();
+    data = { code_available: false };
+  }
+
+  return data;
+};
